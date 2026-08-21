d8bccea1af93a56b3b331a45d5fcdef1d8d73fda
jbolduc <jbolduc@genetec.com>
2026-08-21 14:42:51 -0400

    line numbers are more apparent

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -6,15 +6,15 @@ vim.opt.cursorlineopt = "both"
 vim.opt.cursorcolumn = false
 
 vim.api.nvim_set_hl(0, "LineNr", {
-	fg = "#222222",
+	fg = "#777777",
 })
 
 vim.api.nvim_set_hl(0, "LineNrAbove", {
-	fg = "#222222",
+	fg = "#777777",
 })
 
 vim.api.nvim_set_hl(0, "LineNrBelow", {
-	fg = "#222222",
+	fg = "#777777",
 })
 
 vim.api.nvim_set_hl(0, "CursorLine", {
