1ec8078de1d90eaff2d8179337c998c7058067bf
jbolduc <jbolduc@genetec.com>
2026-08-21 13:21:42 -0400

    Highlight yank and cursorline highlight

--- nvim/.config/nvim/lua/options.lua
+++ nvim/.config/nvim/lua/options.lua
@@ -1,18 +1,22 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
-vim.opt.cursorline = true
-vim.opt.cursorlineopt = "number"
 vim.opt.showmode = false
+vim.opt.cursorline = true
+vim.opt.cursorlineopt = "both"
+vim.opt.cursorcolumn = false
 
 vim.api.nvim_set_hl(0, "LineNr", {
-	fg = "#999999",
+	fg = "#222222",
 })
 
 vim.api.nvim_set_hl(0, "LineNrAbove", {
-	fg = "#999999",
+	fg = "#222222",
 })
 
 vim.api.nvim_set_hl(0, "LineNrBelow", {
-	fg = "#999999",
+	fg = "#222222",
 })
 
+vim.api.nvim_set_hl(0, "CursorLine", {
+	bg = "#222222",
+})
